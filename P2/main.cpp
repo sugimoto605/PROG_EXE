94d4800656f00a35a742f15f6261f252522fea1e
Hiroshi Sugimoto <hsugimot@icloud.com>
2025-05-25 13:58:52 +0900

    VSCodeで変更したぞこのやろう

--- P2/main.cpp
+++ P2/main.cpp
@@ -2,8 +2,8 @@
 #include "engine.hpp"   //仕様書を読み込め
 int main() {
     ENGINE E1,E2;
-    E1.name="engine-1";
-    E2.name="engine-2";
+    E1.name="engine:1";
+    E2.name="engine:2";
     E1.i=1000;
     E2.i=2000;
     E1.print();
