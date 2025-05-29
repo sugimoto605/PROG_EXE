b95f5b812377728a44599eabf2f5a9cfaf5ef635
Hiroshi Sugimoto <hsugimot@icloud.com>
2025-05-29 12:49:54 +0900

    こっちmain

--- P4/jacobi.hpp
+++ P4/jacobi.hpp
@@ -1,3 +1,5 @@
+// Jacobi method: This project implements the Jacobi method for solving linear equations
+// Branch Main
 #pragma once // ifdef...とかしなくても, 現代では，これで行けるらしいな
 #include <iostream>
 #include <fstream>
